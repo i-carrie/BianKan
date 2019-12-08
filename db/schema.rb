402adf52eca788677e07ac4a28f8e547ec405cbd
kaoru.i <carriekcn@gmail.com>
2019-12-08 20:22:20 +0900

    add column at users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_111443) do
+ActiveRecord::Schema.define(version: 2019_12_08_112006) do
 
   create_table "topics", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "subject"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_12_08_111443) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "login_type", default: 2, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
