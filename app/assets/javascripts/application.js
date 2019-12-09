f5cf4ba639c51a19f910edae65ea5478a0d9a638
kaoru.i <carriekcn@gmail.com>
2019-12-09 14:25:20 +0900

    created for main tab contents including ggmap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
+//= require moment
+//= require underscore
+//= require gmaps/google
 //= require activestorage
 //= require turbolinks
 //= require_tree .
