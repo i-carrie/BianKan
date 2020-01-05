6d76ea70f0a414aa9b70ab48452870534fcaf67a
kaoru.i <carriekcn@gmail.com>
2020-01-05 15:06:34 +0900

    create for save at db

--- app/controllers/postnews_controller.rb
+++ app/controllers/postnews_controller.rb
@@ -5,4 +5,24 @@ class PostnewsController < ApplicationController
     @topic = topic.where(flag: 1).order('created_at DESC')
   end
 
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.create(info: create_params[:info], subject: create_params[:subject], flag: 1)
+    if @topic.save
+      flash[:notice] = "送信完了しました"
+      redirect_to(postnews_index_path)    
+    else
+      render(new_postnews_path)
+    end
+  end
+
+  private
+  def create_params
+    params.permit(:info, :subject)
+  end
+
+
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,4 @@
 class Topic < ApplicationRecord
+  validates :subject, {length: {maximum: 10}}
+  validates :info, {presence: true, length: {maximum: 300}}
 end
